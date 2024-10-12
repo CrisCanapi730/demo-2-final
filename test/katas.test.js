25bb42c7ff530307caecbe8fc3d1dd1095a9788a
Cristian Canapi <cristiancanapi730@gmail.com>
2024-10-11 23:58:55 -0400

    Ordenar por descripcion test 2

--- test/katas.test.js
+++ test/katas.test.js
@@ -1,26 +1,45 @@
 const assert = require('assert');
 const { Kata, CatalogoKata } = require('../src/katas.js');
 
-
 describe('CatalogoKata', function() {
     describe('#ordenarPorDescripcion()', function() {
-        it('should sort katas by description in alphabetical order', function() {
-        // Crear instancias de Kata
-        const kata1 = new Kata('Kata 1', 'Autor A', 'Descripcion Z', 'Facil');
-        const kata2 = new Kata('Kata 2', 'Autor B', 'Descripcion A', 'Media');
-        const kata3 = new Kata('Kata 3', 'Autor C', 'Descripcion M', 'Dificil');
+        it('debería ordenar las katas por descripción en orden alfabético', function() {
+            // Crear instancias de Kata
+            const kata1 = new Kata('Kata 1', 'Autor A', 'Descripcion Z', 'Facil');
+            const kata2 = new Kata('Kata 2', 'Autor B', 'Descripcion A', 'Media');
+            const kata3 = new Kata('Kata 3', 'Autor C', 'Descripcion M', 'Dificil');
 
-        const catalogo = new CatalogoKata();
-        catalogo.agregarKata(kata1);
-        catalogo.agregarKata(kata2);
-        catalogo.agregarKata(kata3);
+            const catalogo = new CatalogoKata();
+            catalogo.agregarKata(kata1);
+            catalogo.agregarKata(kata2);
+            catalogo.agregarKata(kata3);
 
-        catalogo.ordenarPorDescripcion();
+            catalogo.ordenarPorDescripcion();
 
-        const listaOrdenada = catalogo.getLista();
-        assert.strictEqual(listaOrdenada[0].getDescripcion(), 'Descripcion A');
-        assert.strictEqual(listaOrdenada[1].getDescripcion(), 'Descripcion M');
-        assert.strictEqual(listaOrdenada[2].getDescripcion(), 'Descripcion Z');
+            const listaOrdenada = catalogo.getLista();
+            assert.strictEqual(listaOrdenada[0].getDescripcion(), 'Descripcion A');
+            assert.strictEqual(listaOrdenada[1].getDescripcion(), 'Descripcion M');
+            assert.strictEqual(listaOrdenada[2].getDescripcion(), 'Descripcion Z');
         });
+
+        it('debería ordenar las katas sin distinguir entre mayúsculas y minúsculas', function() {
+            // Crear instancias de Kata con descripciones en mayúsculas y minúsculas
+            const kata1 = new Kata('Kata 1', 'Autor A', 'descripcion z', 'Facil');
+            const kata2 = new Kata('Kata 2', 'Autor B', 'Descripcion A', 'Media');
+            const kata3 = new Kata('Kata 3', 'Autor C', 'Descripcion M', 'Dificil');
+
+            const catalogo = new CatalogoKata();
+            catalogo.agregarKata(kata1);
+            catalogo.agregarKata(kata2);
+            catalogo.agregarKata(kata3);
+
+            catalogo.ordenarPorDescripcion();
+
+            const listaOrdenada = catalogo.getLista();
+            assert.strictEqual(listaOrdenada[0].getDescripcion().toLowerCase(), 'descripcion a');
+            assert.strictEqual(listaOrdenada[1].getDescripcion().toLowerCase(), 'descripcion m');
+            assert.strictEqual(listaOrdenada[2].getDescripcion().toLowerCase(), 'descripcion z');
+        });
+
     });
 });
