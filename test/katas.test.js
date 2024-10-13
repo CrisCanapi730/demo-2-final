0c00572ce44e30e97a46202481596cbe390140aa
santytorrico <santy.torrico@gmail.com>
2024-10-13 13:30:08 -0400

    Add Test for Catalogo

---
+++ test/katas.test.js
@@ -0,0 +1,89 @@
+const assert = require('assert');
+const { Kata, CatalogoKata } = require('../src/katas.js');
+
+describe('CatalogoKata', function() {
+    describe('#ordenarPorDescripcion()', function() {
+        it('debería ordenar las katas por descripción en orden alfabético', function() {
+            // Crear instancias de Kata
+            const kata1 = new Kata('Kata 1', 'Autor A', 'Descripcion Z', 'Facil');
+            const kata2 = new Kata('Kata 2', 'Autor B', 'Descripcion A', 'Media');
+            const kata3 = new Kata('Kata 3', 'Autor C', 'Descripcion M', 'Dificil');
+
+            const catalogo = new CatalogoKata();
+            catalogo.agregarKata(kata1);
+            catalogo.agregarKata(kata2);
+            catalogo.agregarKata(kata3);
+
+            catalogo.ordenarPorDescripcion();
+
+            const listaOrdenada = catalogo.getLista();
+            assert.strictEqual(listaOrdenada[0].getDescripcion(), 'Descripcion A');
+            assert.strictEqual(listaOrdenada[1].getDescripcion(), 'Descripcion M');
+            assert.strictEqual(listaOrdenada[2].getDescripcion(), 'Descripcion Z');
+        });
+
+        it('debería ordenar las katas sin distinguir entre mayúsculas y minúsculas', function() {
+            // Crear instancias de Kata con descripciones en mayúsculas y minúsculas
+            const kata1 = new Kata('Kata 1', 'Autor A', 'descripcion z', 'Facil');
+            const kata2 = new Kata('Kata 2', 'Autor B', 'Descripcion A', 'Media');
+            const kata3 = new Kata('Kata 3', 'Autor C', 'Descripcion M', 'Dificil');
+
+            const catalogo = new CatalogoKata();
+            catalogo.agregarKata(kata1);
+            catalogo.agregarKata(kata2);
+            catalogo.agregarKata(kata3);
+
+            catalogo.ordenarPorDescripcion();
+
+            const listaOrdenada = catalogo.getLista();
+            assert.strictEqual(listaOrdenada[0].getDescripcion().toLowerCase(), 'descripcion a');
+            assert.strictEqual(listaOrdenada[1].getDescripcion().toLowerCase(), 'descripcion m');
+            assert.strictEqual(listaOrdenada[2].getDescripcion().toLowerCase(), 'descripcion z');
+        });
+        it('debería manejar descripciones idénticas sin errores', function() {
+            // Crear instancias de Kata con descripciones iguales
+            const kata1 = new Kata('Kata 1', 'Autor A', 'Descripcion A', 'Facil');
+            const kata2 = new Kata('Kata 2', 'Autor B', 'Descripcion A', 'Media');
+            const kata3 = new Kata('Kata 3', 'Autor C', 'Descripcion A', 'Dificil');
+
+            const catalogo = new CatalogoKata();
+            catalogo.agregarKata(kata1);
+            catalogo.agregarKata(kata2);
+            catalogo.agregarKata(kata3);
+
+            catalogo.ordenarPorDescripcion();
+
+            const listaOrdenada = catalogo.getLista();
+            assert.strictEqual(listaOrdenada[0].getDescripcion(), 'Descripcion A');
+            assert.strictEqual(listaOrdenada[1].getDescripcion(), 'Descripcion A');
+            assert.strictEqual(listaOrdenada[2].getDescripcion(), 'Descripcion A');
+        });
+        it('debería ordenar las katas con caracteres especiales', function() {
+            
+            const kata1 = new Kata('Kata 1', 'Autor A', 'Descripcion #1', 'Facil');
+            const kata2 = new Kata('Kata 2', 'Autor B', 'Descripcion A', 'Media');
+            const kata3 = new Kata('Kata 3', 'Autor C', 'Descripcion M', 'Dificil');
+
+            const catalogo = new CatalogoKata();
+            catalogo.agregarKata(kata1);
+            catalogo.agregarKata(kata2);
+            catalogo.agregarKata(kata3);
+
+            catalogo.ordenarPorDescripcion();
+
+            const listaOrdenada = catalogo.getLista();
+            assert.strictEqual(listaOrdenada[0].getDescripcion(), 'Descripcion #1');
+            assert.strictEqual(listaOrdenada[1].getDescripcion(), 'Descripcion A');
+            assert.strictEqual(listaOrdenada[2].getDescripcion(), 'Descripcion M');
+        });
+        it('debería manejar una lista vacía sin errores', function() {
+            const catalogo = new CatalogoKata();
+
+            // Intentar ordenar una lista vacía
+            catalogo.ordenarPorDescripcion();
+
+            const listaOrdenada = catalogo.getLista();
+            assert.strictEqual(listaOrdenada.length, 0);  // La lista debería seguir vacía
+        });
+    });
+});
