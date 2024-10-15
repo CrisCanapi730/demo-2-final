4ee31f653b4a750917f50f68a698523523d1d4b0
Osber <osberriojalopez@gmail.com>
2024-10-15 11:42:19 -0400

    usuario.js - deberia retornar el nombre correcto

--- test/usuario.test.js
+++ test/usuario.test.js
@@ -1,2 +1,10 @@
 const assert = require('assert');
-const { Kata, CatalogoKata } = require('../src/usuario.js');
+const { Usuario, Estudiante, Docente } = require('../src/usuario.js');
+
+
+describe('Usuario', () => {
+    it('debería retornar el nombre correcto', () => {
+        const usuario = new Usuario('Carlos', 'Usuario de prueba');
+        assert.strictEqual(usuario.getNombre(), 'Carlos');
+    });
+});
